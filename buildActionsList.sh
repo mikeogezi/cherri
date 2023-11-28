8a46c6116630c20ac645d41d03fd7690990c6dac
Michael Ogezi <mikeogezi@outlook.com>
2023-11-28 07:59:06 -0700

    Update buildActionsList.sh

--- buildActionsList.sh
+++ buildActionsList.sh
@@ -7,6 +7,9 @@ output_file="main_without_main.go"
 # Remove the main function and create a temp file
 sed '/func main()/,/^}/d' "$input_file" > "$output_file"
 
+# lint the temporary file
+goimports -w "$output_file"
+
 # Run go with all .go files in the current directory, excluding main.go and *_test.go files
 go run $(find . -maxdepth 1 -name "*.go" ! -name "main.go" ! -name "*_test.go")
 
